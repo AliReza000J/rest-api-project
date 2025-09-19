7775c8fbe16442eb0b105b0e0bda1d20aa20ec6e
AliReza000J <alirezaabri61@gmail.com>
2025-09-20 00:36:27 +0330

    Email api added :wq

--- resouces/user.py
+++ resouces/user.py
@@ -3,26 +3,56 @@ from flask.views import MethodView
 from flask_smorest import Blueprint
 from passlib.hash import pbkdf2_sha256
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, get_jwt, jwt_required
+import requests
+import os
+from sqlalchemy import or_ 
 
 from blocklist import BLOCKLIST
 from db import db
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema, UserRegisterSchema
 
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN_NAME")
+    return requests.post(
+  		f"https://api.mailgun.net/v3/{domain}/messages",
+  		auth=("api", os.getenv('MAILGUN_API_KEY')),
+  		data={"from": f"Alireza Abri <postmaster@{domain}>",
+			"to": [to],
+  			"subject": subject,
+  			"text": body})
+
 
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"]
+            )
+        ).first():
+            abort(
+                409, message="A user with that username or email already exists!!"
+            )
+            
         user = UserModel(
             username=user_data["username"],
+            email=user_data["email"],
             password=pbkdf2_sha256.hash(user_data["password"])
         )
         db.session.add(user)
         db.session.commit()
+
+        send_simple_message(
+            to=user.email,
+            subject="Succesfully signed up",
+            body=f"Hi {user.username}! You have successfully signed up to the Stores REST API."
+        )
 
         return {"message": "User created successfully"}, 201
     
